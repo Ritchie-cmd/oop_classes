5f171261b5ae2749d2bc3ae05aab37e592645c68
Ritchie-cmd <ritchiebromagos7@gmail.com>
2025-08-13 22:44:05 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'animal.dart';
 import 'phone.dart';
+import 'coffee.dart'; 
 
 void main() {
+  
   Animal hayof = Animal(animal: 'dog', breed: 'pomeranian');
   hayof.showInfo();
-
+  
+    
   Phone hehe = Phone(brand: 'vivo', os: 'android');
   hehe.showInfo();
+  
+  
+  Coffee kape = Coffee(type: 'latte', size: 'large');
+  kape.showInfo();
 }
