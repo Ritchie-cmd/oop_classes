bd8bf614e5244dbec34698d723f113548db10009
Ritchie-cmd <ritchiebromagos7@gmail.com>
2025-08-13 22:40:40 +0800

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
 import 'animal.dart';
 import 'phone.dart';
-import 'coffee.dart';  // Add this line
+import 'coffee.dart'; 
 
 void main() {
-  // Animal code
+  
   Animal hayof = Animal(animal: 'dog', breed: 'pomeranian');
   hayof.showInfo();
   
-  // Phone code  
+    
   Phone hehe = Phone(brand: 'vivo', os: 'android');
   hehe.showInfo();
   
-  // Coffee code (add this)
+  
   Coffee kape = Coffee(type: 'latte', size: 'large');
   kape.showInfo();
 }
